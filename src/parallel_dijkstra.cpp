17b4d66580afd59ed6ef9d58dc8dabbbb49fd7b1
mBlumTechnion <82164285+mBlumTechnion@users.noreply.github.com>
2022-06-12 12:01:53 +0300

    progressed a tiny bit, adding threads

--- src/parallel_dijkstra.cpp
+++ src/parallel_dijkstra.cpp
@@ -9,13 +9,16 @@
 #include <thread>
 #include <numeric>
 #include <boost/pending/relaxed_heap.hpp>
+#include <iterator>
 
-
-//the algorithm keeps a global array for all tentative distance values
-//each processor is responsible for two sequential priority queues
 using std::vector;
 using std::pair;
- 
+//the algorithm keeps a global array for all tentative distance values
+//each processor is responsible for two sequential priority queues
+
+ vector<int> tent;
+
+
 // typedef relaxed_heap<Vertex, IndirectCmp, IndexMap> MutableQueue;
 
 
@@ -41,17 +44,55 @@ class Graph{
         }
 };
 
+std::pair<vector<int>,vector<int>> parallel_dijkstra (Graph g, int source,
+auto start, auto end );
 
-std::pair<vector<int>,vector<int>> p_dijkstra(Graph g, int source){
-    relaxed_heap 
+// Num SumParallel(NumIter begin, NumIter end, Num f(Num), size_t num_threads) {
+//     // YOUR CODE HERE
+//     unsigned long const length = std::distance(begin,end);//length of the vector
+//     if(!length) return 0.;
+
+//     unsigned long const block_size = length/num_threads;
+
+//     std::vector<Num> results(num_threads, 0.);
+//     std::vector<std::thread> workers(num_threads - 1);
+//     NumIter start_block = begin;
+//     for(size_t i=0; i<num_threads-1; ++i){
+//         NumIter end_block = start_block + block_size; 
+//         workers[i] = std::thread(&SumMapThread, start_block, end_block,f,
+//         std::ref(results[i]));
+//         start_block = end_block;
+//     }
+//     SumMapThread(start_block, end,f, results[num_threads - 1]);
+//     for (size_t i = 0; i < num_threads - 1; ++i) {
+//         workers[i].join();
+//     }
+
+//     Num total_result = 0.;
+//     for (size_t i = 0; i < results.size(); ++i) {
+//         total_result += results[i];
+//     }
+//     return total_result;
+//     return 0;
+// }
+
+
+
+void parallel_coordinator(size_t num_threads,Graph g, int source){
+    
+    unsigned long const block_size = tent.size()/num_threads;//do we add -1 to vectors?
+    auto startIter = tent.begin();
+    
+    vector<std::thread> workers(num_threads - 1);//excluding the thread we 
+    //currently run in
+
+    for(size_t i=0; i<num_threads; ++i){
+        auto endIter = startIter + block_size;
+        workers[i] = std::thread(&parallel_dijkstra,g,source,startIter,endIter);
+        startIter = endIter;
+    } 
 
 
 
 }
 
-for(size_t i=0; i<num_threads-1; ++i){
-        NumIter end_block = start_block + block_size; 
-        workers[i] = std::thread(&SumMapThread, start_block, end_block,f,
-        std::ref(results[i]));
-        start_block = end_block;
-    }
