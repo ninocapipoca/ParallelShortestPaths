48479de0fa5f610ad247d5a975602704a393f22a
Carolina <carolina@ninamatos.com>
2022-06-17 22:39:41 +0200

    adding beginning of testing

---
+++ testing/automatic_test.cpp
@@ -0,0 +1,15 @@
+// class Graph{
+//     public:
+//         vector<vector<std::pair<int, int>>> adj;
+//         int nNodes;
+//         void generateGraph(vector<vector<int>> &input, int n){
+//             nNodes = n;
+//             adj.resize(nNodes+1);
+//             for(int i=0; i<input.size(); i++){
+//                 adj[input[i][0]].push_back({input[i][1], input[i][2]});
+//             }
+//         }
+//         void clear(){
+//             adj.clear();
+//         }
+// };
